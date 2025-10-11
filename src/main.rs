dfa2f30d8304b008b8a09be058071b2867b71195
Colin Gilbert <colingilbert86@gmail.com>
2025-10-11 10:31:31 -0600

    "initial" commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::time::Duration;
+use dmc::{octree::*, duals::*};
+use ozz_animation_rs::math::f32_cos;
+use ozz_animation_rs::*;
+use std::collections::*;
+use winit::event_loop::*;
+use noobwerkz::windowing::*;
+fn main() {
+    // wgpu uses `log` for all of our logging, so we initialize a logger with the `env_logger` crate.
+    //
+    // To change the log level, set the `RUST_LOG` environment variable. See the `env_logger`
+    // documentation for more information.
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+
+    // When the current loop iteration finishes, immediately begin a new
+    // iteration regardless of whether or not new events are available to
+    // process. Preferred for applications that want to render as fast as
+    // possible, like games.
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    // When the current loop iteration finishes, suspend the thread until
+    // another event arrives. Helps keeping CPU utilization low if nothing
+    // is happening, which is preferred if the application might be idling in
+    // the background.
+    // event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+}
