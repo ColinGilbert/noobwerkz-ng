f014793998da2ab033a8cef02fded4bc2d08a0cc
Colin Gilbert <colingilbert86@gmail.com>
2025-10-25 13:23:39 -0600

    Continue with 3d model code

--- src/passes/mod.rs
+++ src/passes/mod.rs
@@ -2,6 +2,7 @@ use crate::model_node::ModelNode;
 use crate::model::*;
 
 pub mod forward;
+
 pub trait Pass {
     fn draw(&mut self, device: &wgpu::Device, queue: &wgpu::Queue, models: &Vec<Model>, nodes: &Vec<ModelNode>, depth_texture_view: &wgpu::TextureView, view: &wgpu::TextureView );
 }
