14eb2a0310e31e600649c7ca2e8d164a0c358743
Colin Gilbert <colingilbert86@gmail.com>
2025-10-26 13:25:31 -0600

    Continue with 3d model code

--- src/instance.rs
+++ src/instance.rs
@@ -100,7 +100,7 @@ impl Vertex for InstanceRaw {
 impl Vertex for SkinnedInstanceRaw {
     fn desc() -> wgpu::VertexBufferLayout<'static> {
         wgpu::VertexBufferLayout {
-            array_stride: mem::size_of::<InstanceRaw>() as wgpu::BufferAddress,
+            array_stride: mem::size_of::<SkinnedInstanceRaw>() as wgpu::BufferAddress,
             // We need to switch from using a step mode of Vertex to Instance
             // This means that our shaders will only change to use the next
             // instance when the shader starts processing a new instance
