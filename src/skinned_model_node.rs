0046e8e265279a3fc333e834acdd50ad5d92aee4
Colin Gilbert <colingilbert86@gmail.com>
2025-10-29 21:11:41 -0600

    continue with skeletal mesh rendering

--- src/skinned_model_node.rs
+++ src/skinned_model_node.rs
@@ -105,14 +105,14 @@ impl SkinnedModelNode {
             for b in bone_transforms {
                 self.bone_matrices.push(AnimationMatrix {
                     data: (glam::Mat4::IDENTITY
+                        * glam::Mat4::from_translation(b.position)
+                        * glam::Mat4::from_quat(b.rotation)
                         * glam::Mat4::from_scale(glam::Vec3 {
                             x: b.scale,
                             y: b.scale,
                             z: b.scale,
-                        })
-                        * glam::Mat4::from_quat(b.rotation)
-                        * glam::Mat4::from_translation(b.position))
-                    .to_cols_array_2d(),
+                        }))
+                    .to_cols_array_2d()
                 });
             }
         }
