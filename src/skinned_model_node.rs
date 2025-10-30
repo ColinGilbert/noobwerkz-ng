c617c0257d1266d5a7f1bb6ad62fd1a5393787da
Colin Gilbert <colingilbert86@gmail.com>
2025-10-29 22:23:45 -0600

    continue with skeletal mesh rendering

--- src/skinned_model_node.rs
+++ src/skinned_model_node.rs
@@ -25,7 +25,7 @@ impl SkinnedModelNode {
     ) -> Self {
         let mut playbacks = Vec::new();
         let skeleton = skeletal_context.skeleton.clone();
-        let num_bones = skeleton.num_joints() as u32 - 1;
+        let num_bones = skeleton.num_joints() as u32;
         let animation = skeletal_context.animations[0].clone();
         let len = instances.len();
         let mut bone_matrices = Vec::new();
@@ -42,7 +42,7 @@ impl SkinnedModelNode {
 
         for p in &mut playbacks {
             p.update(web_time::Duration::from_secs(0));
-            let bone_transforms = p.bone_trans();
+            let bone_transforms = p.spine_trans();
             //let mut i = 0;
             for b in bone_transforms {
                 bone_matrices.push(AnimationMatrix {
