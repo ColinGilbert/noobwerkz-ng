08c11f9c1f41f901e8fb7afbebf3ad8860b2bfde
Colin Gilbert <colingilbert86@gmail.com>
2025-10-29 20:52:26 -0600

    continue with skeletal mesh rendering

--- src/state.rs
+++ src/state.rs
@@ -185,7 +185,7 @@ impl State {
     }
 
     pub fn update(&mut self, dt: std::time::Duration) {
-        println!("update dt={}", dt.as_millis());
+        println!("update dt={}", dt.as_secs_f32());
         if let Some(cb) = *USER_UPDATE_CALLBACK.lock().unwrap() {
             cb(
                 &mut self.gfx_ctx,
