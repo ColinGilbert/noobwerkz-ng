b68394bf61b161f9b32da3d48b3116845cd73a25
Colin Gilbert <colingilbert86@gmail.com>
2025-11-03 17:00:10 -0600

    Switch to msgpacker

--- src/skinned_model_node.rs
+++ src/skinned_model_node.rs
@@ -114,7 +114,7 @@ impl SkinnedModelNode {
             for b in bone_transforms {
                 self.bone_matrices.push(BoneMatrix {
                     data: glam::Mat4::from_scale_rotation_translation(glam::Vec3::splat(b.scale), b.rotation, b.position)
-                    .transpose().to_cols_array_2d(),
+                    .to_cols_array_2d(),
                     //data: glam::Mat4::IDENTITY.to_cols_array_2d(),
                 });
             }
