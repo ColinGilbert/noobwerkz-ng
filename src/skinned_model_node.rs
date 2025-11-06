37453dcf54f973ddfcff215684c7baf0c14bb5b6
Colin Gilbert <colingilbert86@gmail.com>
2025-11-05 19:38:48 -0600

    tweaks

--- src/skinned_model_node.rs
+++ src/skinned_model_node.rs
@@ -128,11 +128,11 @@ impl SkinnedModelNode {
             let bone_transforms = p.bone_trans();
             for (i, b) in bone_transforms.iter().enumerate() {
                 self.bone_matrices.push(BoneMatrix {
-                    data: ( glam::Mat4::from_scale_rotation_translation(
+                    data: ( skinned_model.inverse_bind_matrices[i].inverse() * glam::Mat4::from_scale_rotation_translation(
                             glam::Vec3::splat(b.scale),
                             b.rotation,
                             b.position,
-                        ) * skinned_model.inverse_bind_matrices[i])
+                        ))
                     .to_cols_array_2d(),
                     // data: glam::Mat4::IDENTITY.to_cols_array_2d(),
                 });
