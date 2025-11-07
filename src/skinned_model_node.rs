8ef38e7d8622e6ba4dd1f0e0fe299c0a2fba8b62
Colin Gilbert <colingilbert86@gmail.com>
2025-11-07 11:30:46 -0600

    sketal animation worksgit add . && git commit -m tweaks && git push!

--- src/skinned_model_node.rs
+++ src/skinned_model_node.rs
@@ -47,7 +47,7 @@ impl SkinnedModelNode {
             //et bone_transforms = p.bone_trans();
            // println!("Bone transform length {}", bone_transforms.len(),);
             //for _b in bone_transforms {
-            for m in &*(p.models).read().unwrap() {
+            for _m in &*(p.models).read().unwrap() {
                 bone_matrices.push(BoneMatrix {
                     // data: (glam::Mat4::from_scale_rotation_translation(
                     //     glam::Vec3::splat(b.scale),
