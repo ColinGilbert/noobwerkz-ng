0a92ac27e359ec7bdc518c3458003027c8049195
Colin Gilbert <colingilbert86@gmail.com>
2025-11-09 13:35:32 -0600

    enhance primitive code

--- src/primitives.rs
+++ src/primitives.rs
@@ -32,9 +32,9 @@ pub fn cylinder(height: f64, radius: f64) -> SerializedModel {
 }
 
 pub fn cone(height: f64, radius: f64) -> SerializedModel {
-    let v0 = builder::vertex(Point3::new(0.0, -height / 2.0, 0.0));
+    let v0 = builder::vertex(Point3::new(0.0, height / 2.0, 0.0));
     let v1 = builder::vertex(Point3::new(0.0, -height / 2.0, radius));
-    let v2 = builder::vertex(Point3::new(0.0, height / 2.0, 0.0));
+    let v2 = builder::vertex(Point3::new(0.0, -height / 2.0, 0.0));
     let wire: Wire = vec![builder::line(&v0, &v1), builder::line(&v1, &v2)].into();
     let shell = builder::cone(&wire, Vector3::unit_y(), Rad(7.0));
     let cone = Solid::new(vec![shell]);
