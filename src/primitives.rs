b73d1bce64159c59b43d27cd1569dd627f16bd1a
Colin Gilbert <colingilbert86@gmail.com>
2025-11-11 15:10:22 -0600

    experimental support for capsule

--- src/primitives.rs
+++ src/primitives.rs
@@ -77,8 +77,8 @@ pub fn capsule(height: f64, radius: f64) -> SerializedModel {
         &v3,
         Point3::new(
             0.0,
-            libm::sqrt(2.0) / 2.0,
             -libm::sqrt(2.0) / 2.0,
+            libm::sqrt(2.0) / 2.0,
         ),
     );
 
