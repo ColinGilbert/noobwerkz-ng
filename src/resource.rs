a77761f2335e54de218d219fd2115b133df58c02
Colin Gilbert <colingilbert86@gmail.com>
2025-11-12 23:08:06 -0600

    Tweaks

--- src/resource.rs
+++ src/resource.rs
@@ -48,10 +48,12 @@ pub fn load_skinned_model_from_serialized(
         }
         if m.positions.len() != m.bone_indices.len() {
             println!("Not enough bone indices. Indices: {}. Needed: {}.", m.bone_indices.len(), m.positions.len());
+            m.bone_indices.resize(m.positions.len(), [0, 0, 0,0 ]);
             //return Option::None;
         }
         if m.positions.len() != m.bone_weights.len() {
             println!("Not enough bone weights");
+            m.bone_weights.resize(m.positions.len(), [0.0, 0.0, 0.0, 0.0]);
             //return Option::None;
         }
         let mut i = 0;
