ac9c3d0ade77a84d0481c4c1cadc8a612066266f
Colin Gilbert <colingilbert86@gmail.com>
2025-11-17 19:38:16 -0600

    Start adding kira audio

--- src/user_context.rs
+++ src/user_context.rs
@@ -1,11 +1,15 @@
 //use std::sync::*;
 use crate::{asset_manager::*, scene::*, skeletal_context::SkeletalContext};
-
+use kira::{
+	AudioManager, AudioManagerSettings, DefaultBackend,
+	sound::static_sound::{StaticSoundData, StaticSoundSettings},
+};
 
 pub struct UserContext {
     pub asset_mgr: AssetManager,
     pub skeletals: Vec<SkeletalContext>,
     pub scenes: Vec<Scene>,
+    pub audio_mgr: AudioManager,
     pub active_scene: usize,
     pub time_elapsed: u128,
 }
@@ -15,10 +19,12 @@ impl UserContext {
         let asset_mgr = AssetManager::new();
         let skeletals = Vec::new();
         let scenes = Vec::<Scene>::new();
+        let audio_mgr =  AudioManager::<DefaultBackend>::new(AudioManagerSettings::default())?;
         Self {
             asset_mgr,
             skeletals,
             scenes,
+            audio_mgr,
             active_scene: 0,
             time_elapsed: 0,
         }
